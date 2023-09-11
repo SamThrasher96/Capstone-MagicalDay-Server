e3175f98706314f96f388daec0b8a15fd029622b
Sam Thrasher <samthrasher9@gmail.com>
2023-09-11 09:33:03 -0500

    Added models

--- magicaldayapi/models/location_model.py
+++ magicaldayapi/models/location_model.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Location(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=150)
     capacity = models.IntegerField()
     image = models.CharField(max_length=500)
     opening_time = models.TimeField()
