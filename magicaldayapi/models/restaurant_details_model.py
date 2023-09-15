b5f43d80b3d788ac474711dde46c66dba4fbbe44
Sam Thrasher <samthrasher9@gmail.com>
2023-09-15 10:40:55 -0500

    Added in decorators for restaurants

--- magicaldayapi/models/restaurant_details_model.py
+++ magicaldayapi/models/restaurant_details_model.py
@@ -3,3 +3,11 @@ from django.db import models
 class RestaurantDetails(models.Model):
     location = models.ForeignKey("Location", on_delete=models.CASCADE)
     allergy_friendly = models.BooleanField()
+
+    @property
+    def restaurant_name(self):
+        return self.location.name
+
+    @property
+    def restaurant_image(self):
+        return self.location.image
